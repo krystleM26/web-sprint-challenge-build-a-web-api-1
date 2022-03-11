5ee696ed2744cbbe1fcff72961a38837a14723d5
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-03-11 12:04:01 -0800

    Middleware is working

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,20 @@
-// add middlewares here related to projects
+const Projects = require('./projects-model')
+
+async function validateProjectId(req,res,next) {
+    try {
+        const project = await Projects.get(req.params.id)
+        if(!project) {
+            res.status(404).json()
+        } else {
+            req.project
+        }
+    }
+    catch(err) {
+        res.status(500).json()
+    }
+    next()
+}
+
+module.exports = {
+    validateProjectId
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,40 +1,48 @@
 // Write your "projects" router here!
 const express = require('express')
+const {validateProjectId} = require('./projects-middleware')
+const Projects = require('./projects-model')
 const router = express.Router()
 
-const Projects = require('./projects-model')
 
-router.get('/', (req,res,) => {
-    
-    Projects.get()
-    .then(project => {
-        res.status(200).json(project)
+router.get('/', (req, res) => {
+  Projects.get()
+    .then((project) => {
+      res.status(200).json(project)
     })
-    .catch(err => {
-        res.status(500).json({message: 'Cannot get Projects'})
+    .catch((err) => {
+      res.status(500).json({ message: 'Cannot get Projects' })
     })
 })
 
+router.get('/:id', validateProjectId, async (req, res) => {
+    try{
 
-router.get('/:id', (req,res,)=> {
-    Projects.insert()
-    console.log('get projects by id')
+        const project = await Projects.get(req.param.id)
+        if (project) {
+            res.json(project)
+        } else {
+            res.status(404).json({message: 'Cannot find project with that id'})
+        }
+    } catch(err) {
+        res.status(500).json(err)
+    }
 })
 
-router.post('/', (req,res) => {
-console.log('get new project')
+router.post('/', (req, res) => {
+  console.log('get new project')
 })
 
 router.put('/:id', (req, res) => {
-    console.log('get updated project')
+  console.log('get updated project')
 })
 
-router.delete('/:id', (req,rest) => {
-    console.log('delete project')
+router.delete('/:id', (req, rest) => {
+  console.log('delete project')
 })
 
-router.get('/:id/actions', (req,res) => {
-    console.log('get project actions')
+router.get('/:id/actions', (req, res) => {
+  console.log('get project actions')
 })
 
-module.exports= router
+module.exports = router
